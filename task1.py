2fb3872f5ed5fa43e36e1912b037e108ee676819
crictonn <devicejoy@gmail.com>
2022-11-12 00:37:54 +0300

    task 1 finally done...

--- task1.py
+++ task1.py
@@ -1,9 +1,21 @@
 f1 = open("file1.txt", "a")
+f2 = open("file2.txt", "w")
 st = " "
 while st != "":
     st = input("Данные (пустая строка для окончания ввода): ")
-    f1.write(st + "\n")
+    if st != "":
+        f1.write(st + "\n")
 f1 = open("file1.txt", "r")
-data = f1.read()
-print(data)
+for line in f1:
+    a = line.replace("\n", "")
+    k = list(a.replace(" ", ""))
+    check = 0
+    for i in range(len(k)):
+        if k[i].isdigit():
+            check = 1
+        else:
+            continue
+    if check == 0:
+        f2.write(line)
 f1.close()
+f2.close()
--- task2.py
+++ task2.py
@@ -1,5 +1,11 @@
-k = input()
-if k.isdigit():
-    print("AAAAAAAAAA")
-else:
-    print("blya...")
+a = input()
+k = list(a.replace(" ", ""))
+check = 0
+for i in range(len(k)):
+    if k[i].isdigit():
+        print("good")
+    else:
+        check = 1
+    if check == 0:
+        print('Boobs') #Здесь нужно записать строку во второй файл
+print(k, check)
